6e6b5dfa9a245417e57103f523051c72a2782914
William Saidac <sdfsfsds>
2016-11-10 16:23:08 +0100

    complete

--- index.js
+++ index.js
@@ -1,10 +1,38 @@
+// Min version
 const express = require('express'),
       morgan = require('morgan'),
       pug = require('pug');
+      methodOverride = require('method-override');
+      bodyParser = require('body-parser');
+      Sequelize = require('sequelize');
 
 var app = express();
+    sequelize = new Sequelize('wille','wille', '', {dialect: 'postgres' });
+
+var booksRouter = require('./routes/books');
+
+var Book = sequelize.define('book', {
+  title: Sequelize.STRING,
+  imageURL: Sequelize.STRING,
+  author: Sequelize.STRING,
+  description: Sequelize.TEXT
+});
+// ===
+
+
+app.use(express.static('public'));
 
 app.use(morgan('dev'));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(methodOverride((req, res) => {
+  if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+    var method = req.body._method;
+    delete req.body._method;
+    return method;
+  }})
+);
 
 app.set('view engine', 'pug');
 
@@ -12,14 +40,11 @@ app.get('/', (request, response) => {
   response.redirect('/books');
 });
 
-app.get('/books', (request, response) => {
-  response.render('books/index');
-});
+app.use('/books', booksRouter);
 
-app.get('/books/new', (request, response) => {
-  response.render('books/new');
-});
-
-app.listen(3001, () => {
-  console.log('Web Server is running on port 3001');
+sequelize.sync().then(() => {
+  console.log('Connected to db');
+  app.listen(3001, () => {
+    console.log('Web Server is running on port 3001');
+  });
 });
